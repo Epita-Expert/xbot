cb7bfd644df930f9df8fa93e220826f512ab1452
thomas-mrx <thomas.moreaux@icloud.com>
2021-09-16 12:11:44 +0200

    fix: remove deprecated alexa references

--- commands/okbot.js
+++ commands/okbot.js
@@ -1,6 +1,3 @@
-const alexa = require("alexa-bot-api");
-const ai = new alexa();
-
 module.exports = {
     isGlobal: false,
     data: {
@@ -10,9 +7,6 @@ module.exports = {
     execute: async ({ channel, user, client, interaction }) => {
         let message = await channel.messages.fetch(interaction.targetId)
         //console.log(message)
-        //const reply = await ai.getReply("Comment ça va ?", "french");
-
-        //console.log(reply);
         await interaction.reply({ content: 'Bientôt...', ephemeral: true })
     }
 }
