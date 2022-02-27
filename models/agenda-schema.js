2cd89c37a8fc31cdf42ee8b6cb41b14e51b4c043
thomas-mrx <thomas.moreaux@icloud.com>
2022-02-27 09:08:27 +0100

    feat: db connect using env url

--- models/agenda-schema.js
+++ models/agenda-schema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PWD+'@localhost:18888/xbot', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PWD+'@'process.env.DB_URL':18888/xbot?authSource=admin', {useNewUrlParser: true, useUnifiedTopology: true})
 db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => console.log("MongoDB successfully connected") )
