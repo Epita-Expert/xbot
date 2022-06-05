5a3164b503eca3c64ee47ea284316aca2ebeb906
thomas-mrx <thomas.moreaux@icloud.com>
2022-06-05 13:56:21 +0000

    Add CI/CD configuration

---
+++ test/blague.test.js
@@ -0,0 +1,21 @@
+const blague = require('../commands/blague.js')
+const BlaguesAPI = require('blagues-api')
+require('dotenv').config()
+
+describe('blague', () => {
+    it('params verification', () => {
+        expect(blague.data.name).toBe('blague');
+        
+    });
+
+    it('all choices work', () => {
+        blague.data.options.find(e => e.name === "type")?.choices?.forEach(e => { 
+            const type = e.value
+            const blagues = new BlaguesAPI(process.env.BLAGUES_TOKEN);
+            return blagues.randomCategorized(blagues.categories[type]).then(data => {
+                expect(data.type).toBe(e.value.toLowerCase());
+            });
+          
+        })
+    })
+})
---
+++ test/pierre-papier-ciseaux.test.js
@@ -0,0 +1,9 @@
+const pierre_papier_ciseaux = require('../commands/pierre-papier-ciseaux')
+
+describe('pierre-papier-ciseaux', () => {
+    it('params verification', ()=> {
+      expect(pierre_papier_ciseaux.data.name).toBe('Pierre-papier-ciseaux');
+    });
+
+
+})
