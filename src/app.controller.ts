8c7e338bde38a766ed18b740d34a6de07d2c2d00
Minaro <paturetromain@gmail.com>
2022-11-15 14:33:39 +0100

    Add privacy policy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('privacy-policy')
+  getPrivacyPolicy(): string {
+    return this.appService.getPrivacyPolicy();
+  }
 }
